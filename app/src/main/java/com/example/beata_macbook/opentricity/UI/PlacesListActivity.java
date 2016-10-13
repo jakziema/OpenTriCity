adac427fd568980fe287f0b2284e0c5c928040ee
Jakub Ziemann <ziemannjakub@gmail.com>
2016-10-13 12:36:01 +0200

    Biblioteki do FirebaseDatabse i UI

---
+++ app/src/main/java/com/example/beata_macbook/opentricity/UI/PlacesListActivity.java
@@ -0,0 +1,20 @@
+package com.example.beata_macbook.opentricity.UI;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+import com.example.beata_macbook.opentricity.R;
+
+/**
+ * Created by Beata-MacBook on 13.10.2016.
+ */
+
+public class PlacesListActivity extends AppCompatActivity {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_places_list);
+
+
+    }
+}
