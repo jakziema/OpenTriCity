a741ff6a0f1f5410d5e75b9e821eb068955f3c6f
Jakub Ziemann <ziemannjakub@gmail.com>
2016-10-26 18:12:00 +0200

    Dzialace wyszukiwanie po kateogrii niepelnosprawnosci

--- app/src/main/java/com/example/beata_macbook/opentricity/UI/UI/CategoriesScreenActivity.java
+++ app/src/main/java/com/example/beata_macbook/opentricity/UI/UI/CategoriesScreenActivity.java
@@ -1,9 +1,13 @@
 package com.example.beata_macbook.opentricity.UI.UI;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
+import android.widget.CompoundButton;
+import android.widget.ToggleButton;
 
 import com.example.beata_macbook.opentricity.R;
 
@@ -17,6 +21,13 @@ import com.example.beata_macbook.opentricity.R;
  */
 public class CategoriesScreenActivity extends AppCompatActivity {
 
+    ToggleButton wheelchairToggleButton;
+    ToggleButton electronicWheelchairToggleButton;
+    ToggleButton crutchesToggleButton;
+
+    public static String toggleButtonChoice = "";
+
+
     // zmienna globalna wybor, która pozwala nam na ustawienie odpowiedniej kategorii do bazy Firebase
     public static String choice = "";
 
@@ -24,6 +35,52 @@ public class CategoriesScreenActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_category_screen);
+
+        wheelchairToggleButton = (ToggleButton)findViewById(R.id.wheelchairToggleButton);
+        electronicWheelchairToggleButton = (ToggleButton)findViewById(R.id.electronicWheelchairToggleButton);
+        crutchesToggleButton = (ToggleButton)findViewById(R.id.crunchesToggleButton);
+
+        wheelchairToggleButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(wheelchairToggleButton.isChecked()) {
+                    wheelchairToggleButton.setBackgroundColor(Color.RED);
+                    toggleButtonChoice = "wheelchair";
+                    Log.v("Wcisnieto", toggleButtonChoice);
+                }else {
+                    wheelchairToggleButton.setBackgroundColor(Color.GRAY);
+                }
+            }
+        });
+
+
+
+        electronicWheelchairToggleButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(electronicWheelchairToggleButton.isChecked()) {
+                    electronicWheelchairToggleButton.setBackgroundColor(Color.RED);
+                    toggleButtonChoice = "el_wheelchair";
+                    Log.v("Wcisnieto", toggleButtonChoice);
+                }else {
+                    electronicWheelchairToggleButton.setBackgroundColor(Color.GRAY);
+                }
+            }
+        });
+
+
+        crutchesToggleButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (crutchesToggleButton.isChecked()) {
+                    crutchesToggleButton.setBackgroundColor(Color.RED);
+                    toggleButtonChoice  = "crutch";
+                    Log.v("Wcisnieto", toggleButtonChoice);
+                } else {
+                    crutchesToggleButton.setBackgroundColor(Color.GRAY);
+                }
+            }
+        });
     }
 
     public void onClick(View view) {
@@ -50,12 +107,14 @@ public class CategoriesScreenActivity extends AppCompatActivity {
                 wybrano = "Urzedy";
                 choice= "Urzedy";
                 break;
+            default:
+                break;
         }
 
         Intent intent = new Intent(this, PlacesListActivity.class);
         //przesylamy wybor uzytkownika
         intent.putExtra("chosenCategory", wybrano);
+        intent.putExtra("toggleButtonChoice", toggleButtonChoice);
         startActivity(intent);
-
     }
 }
