36ffaf4492cdc1022e5708da9b478a5d27dba2f4
kponiatowska <katarzynaponiatowska94@gmail.com>
2016-11-21 18:22:19 +0100

    Aplikacja z funkcją dodawania miejsca

--- app/src/main/java/com/example/beata_macbook/opentricity/UI/Adapter/CustomCommentsAdapter.java
+++ app/src/main/java/com/example/beata_macbook/opentricity/UI/Adapter/CustomCommentsAdapter.java
@@ -12,6 +12,7 @@ import android.widget.Button;
 import android.widget.ListView;
 import android.widget.TextView;
 
+
 import com.example.beata_macbook.opentricity.R;
 import com.example.beata_macbook.opentricity.UI.Model.Comment;
 import com.example.beata_macbook.opentricity.UI.Utils.EmailSender;
--- app/src/main/java/com/example/beata_macbook/opentricity/UI/UI/AddPlace.java
+++ app/src/main/java/com/example/beata_macbook/opentricity/UI/UI/AddPlace.java
@@ -1,24 +1,27 @@
 package com.example.beata_macbook.opentricity.UI.UI;
 
-import android.content.DialogInterface;
-import android.content.Intent;
-import android.location.Location;
-import android.location.LocationListener;
-import android.location.LocationManager;
+
 import android.net.Uri;
 import android.os.Bundle;
-import android.provider.Settings;
-import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.Toast;
 import android.widget.ToggleButton;
 
 
 import com.example.beata_macbook.opentricity.R;
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.appindexing.Thing;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.squareup.picasso.Picasso;
@@ -31,10 +34,12 @@ import org.parceler.Parcels;
 import java.util.Random;
 
 import static android.R.attr.category;
+import static android.R.attr.text;
 import static com.example.beata_macbook.opentricity.UI.UI.CategoriesScreenActivity.wyborNiepelnosprawnosci;
 
 public class AddPlace extends AppCompatActivity {
 
+    String text;
     EditText mTextName;
     EditText mTextAddress;
     EditText mTextPhone;
@@ -42,12 +47,19 @@ public class AddPlace extends AppCompatActivity {
     EditText mTextStaff;
     EditText mTextToilets;
     EditText mTextRamp;
+    Spinner spinner;
+    ArrayAdapter<CharSequence> adapter;
 
     ToggleButton elevatorBtn;
 
     Button mAddBtn;
     private String category;
-
+    private FirebaseAuth mAuth;
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    private GoogleApiClient client;
 
     //DatabaseReference mRootRef = FirebaseDatabase.getInstance().getReference();
     //id = Parcels.unwrap(getIntent().getParcelableExtra("id"));
@@ -63,37 +75,64 @@ public class AddPlace extends AppCompatActivity {
         mTextAddress = (EditText) findViewById(R.id.address);
         mTextPhone = (EditText) findViewById(R.id.phone);
         mTextElevator = (EditText) findViewById(R.id.elevator);
-        mTextStaff = (EditText)findViewById(R.id.staff);
-        mTextToilets = (EditText)findViewById(R.id.toilets);
-        mTextRamp = (EditText)findViewById(R.id.ramp);
+        mTextStaff = (EditText) findViewById(R.id.staff);
+        mTextToilets = (EditText) findViewById(R.id.toilets);
+        mTextRamp = (EditText) findViewById(R.id.ramp);
 
         mAddBtn = (Button) findViewById(R.id.addBtn);
+
+        spinner = (Spinner) findViewById(R.id.spinner);
+        adapter = ArrayAdapter.createFromResource(this, R.array.categories, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            public void onItemSelected(AdapterView<?> parent, View view,
+                                       int position, long id) {
+                parent.getItemAtPosition(position);
+                text = parent.getSelectedItem().toString();
+
+
+            }
+
+            public void onNothingSelected(AdapterView<?> parent) {
+                // Another interface callback
+            }
+        });
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
     }
     //category = Parcels.unwrap(getIntent().getParcelableExtra("category"));
 
 
-
     private void addNewPlace() {
+
         Random rand = new Random();
 
-        int  n = rand.nextInt(1000) + 1;
-        DatabaseReference ref = FirebaseDatabase.getInstance().getReference().child("Lokale_uslugowe").child(String.valueOf(n)).child("name");
-        DatabaseReference refAdr = FirebaseDatabase.getInstance().getReference().child("Lokale_uslugowe").child(String.valueOf(n)).child("address");
-        DatabaseReference refPho = FirebaseDatabase.getInstance().getReference().child("Lokale_uslugowe").child(String.valueOf(n)).child("phoneNumber");
-        DatabaseReference refEle = FirebaseDatabase.getInstance().getReference().child("Lokale_uslugowe").child(String.valueOf(n)).child("elevator");
-        DatabaseReference refStaff = FirebaseDatabase.getInstance().getReference().child("Lokale_uslugowe").child(String.valueOf(n)).child("staff");
-        DatabaseReference refToil = FirebaseDatabase.getInstance().getReference().child("Lokale_uslugowe").child(String.valueOf(n)).child("toilets");
-        DatabaseReference refRamp = FirebaseDatabase.getInstance().getReference().child("Lokale_uslugowe").child(String.valueOf(n)).child("podjazdy");
+        int n = rand.nextInt(1000) + 1;
+
+        DatabaseReference ref = FirebaseDatabase.getInstance().getReference().child(text).child(String.valueOf(n)).child("name");
+        DatabaseReference refAdr = FirebaseDatabase.getInstance().getReference().child(text).child(String.valueOf(n)).child("address");
+        DatabaseReference refPho = FirebaseDatabase.getInstance().getReference().child(text).child(String.valueOf(n)).child("phoneNumber");
+        DatabaseReference refEle = FirebaseDatabase.getInstance().getReference().child(text).child(String.valueOf(n)).child("elevator");
+        DatabaseReference refStaff = FirebaseDatabase.getInstance().getReference().child(text).child(String.valueOf(n)).child("staff");
+        DatabaseReference refToil = FirebaseDatabase.getInstance().getReference().child(text).child(String.valueOf(n)).child("toilets");
+        DatabaseReference refRamp = FirebaseDatabase.getInstance().getReference().child(text).child(String.valueOf(n)).child("podjazdy");
+        Log.d("Wybrałam", text);
+
 
         final String name = mTextName.getText().toString();
         final String address = mTextAddress.getText().toString();
         final String phone = mTextPhone.getText().toString();
+        String phonePatter = "[0-9]+[0-9]+[0-9]+[0-9]";
         final String elevator = mTextElevator.getText().toString();
         final String staff = mTextStaff.getText().toString();
         final String ramp = mTextRamp.getText().toString();
         final String toilets = mTextToilets.getText().toString();
 
-        if (!name.isEmpty() && !address.isEmpty() && !phone.isEmpty() && !elevator.isEmpty()) {
+        if (!name.isEmpty() && !address.isEmpty() && !phone.isEmpty() && !elevator.isEmpty() && phone.matches(phonePatter)) {
             ref.setValue(name);
             refAdr.setValue(address);
             refPho.setValue(phone);
@@ -103,17 +142,53 @@ public class AddPlace extends AppCompatActivity {
             refRamp.setValue(ramp);
             finish();
             startActivity(getIntent());
+        } else {
+            Toast.makeText(AddPlace.this,
+                    "Uzupełnij wszystkie pola", Toast.LENGTH_LONG).show();
         }
+
     }
 
     @Override
     protected void onStart() {
-        super.onStart();
+        super.onStart();// ATTENTION: This was auto-generated to implement the App Indexing API.
+// See https://g.co/AppIndexing/AndroidStudio for more information.
+        client.connect();
 
         mAddBtn.setOnClickListener(new View.OnClickListener() {
             public void onClick(View view) {
                 addNewPlace();
+                Log.d("Wybrano", text);
             }
         });
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        AppIndex.AppIndexApi.start(client, getIndexApiAction());
+    }
+
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    public Action getIndexApiAction() {
+        Thing object = new Thing.Builder()
+                .setName("AddPlace Page") // TODO: Define a title for the content shown.
+                // TODO: Make sure this auto-generated URL is correct.
+                .setUrl(Uri.parse("http://[ENTER-YOUR-URL-HERE]"))
+                .build();
+        return new Action.Builder(Action.TYPE_VIEW)
+                .setObject(object)
+                .setActionStatus(Action.STATUS_TYPE_COMPLETED)
+                .build();
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        AppIndex.AppIndexApi.end(client, getIndexApiAction());
+        client.disconnect();
     }
 }
