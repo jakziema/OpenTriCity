7fd8b75d2fc149d0b5590538eeeb38e047469e9f
Jakub Ziemann <ziemannjakub@gmail.com>
2016-11-28 16:17:35 +0100

    Poprawiony wyglad

--- app/src/main/java/com/example/beata_macbook/opentricity/UI/UI/ListCategoriesScreenActivity.java
+++ app/src/main/java/com/example/beata_macbook/opentricity/UI/UI/ListCategoriesScreenActivity.java
@@ -1,6 +1,7 @@
 package com.example.beata_macbook.opentricity.UI.UI;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
@@ -9,8 +10,13 @@ import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.Spinner;
+import android.widget.Toast;
 
 import com.example.beata_macbook.opentricity.R;
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.appindexing.Thing;
+import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -18,6 +24,7 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 /**
@@ -33,21 +40,13 @@ public class ListCategoriesScreenActivity extends AppCompatActivity {
     public static String choice = "";
     public static String wyborNiepelnosprawnosci = "";
 
-
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_categorylist_screen);
 
-        categorySpinner = (Spinner)findViewById(R.id.categorySpinner);
-        disabilitySpinner = (Spinner)findViewById(R.id.disabilitySpinner);
-        searchButton = (Button)findViewById(R.id.searchButton);
-
-        categorySpinner.setPrompt("Wybierz rodzaj lokalu");
-        disabilitySpinner.setPrompt("Wybierz rodzaj niepełnosprawności");
+        searchButton = (Button) findViewById(R.id.searchButton);
 
 
-        final List<String> categories = new ArrayList<String>();
-        final List<String> disabilities = new ArrayList<String>();
 
         final FirebaseDatabase database = FirebaseDatabase.getInstance();
         DatabaseReference myRefCategories = database.getReference("Kategorie");
@@ -55,13 +54,32 @@ public class ListCategoriesScreenActivity extends AppCompatActivity {
         myRefCategories.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
-                for(DataSnapshot ds: dataSnapshot.getChildren()) {
-
+                final List<String> categories = new ArrayList<String>();
+                for (DataSnapshot ds : dataSnapshot.getChildren()) {
                     for (DataSnapshot ds2 : ds.getChildren()) {
                         Log.v("Categories", ds2.getValue().toString());
                         categories.add(ds2.getValue().toString());
                     }
                 }
+
+                categorySpinner = (Spinner) findViewById(R.id.categorySpinner);
+                ArrayAdapter<String> categoryAdapter = new ArrayAdapter<String>(ListCategoriesScreenActivity.this,
+                        android.R.layout.simple_spinner_item, categories);
+                categoryAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+                categorySpinner.setAdapter(categoryAdapter);
+
+                categorySpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+                    @Override
+                    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                        choice = parent.getItemAtPosition(position).toString();
+
+                    }
+
+                    @Override
+                    public void onNothingSelected(AdapterView<?> parent) {
+
+                    }
+                });
             }
 
             @Override
@@ -74,13 +92,36 @@ public class ListCategoriesScreenActivity extends AppCompatActivity {
         myRefDisabilities.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
-                for(DataSnapshot ds: dataSnapshot.getChildren()) {
+
+                final List<String> disabilities = new ArrayList<String>();
+                for (DataSnapshot ds : dataSnapshot.getChildren()) {
 
                     for (DataSnapshot ds2 : ds.getChildren()) {
                         Log.v("Categories", ds2.getValue().toString());
                         disabilities.add(ds2.getValue().toString());
                     }
                 }
+
+                disabilitySpinner = (Spinner) findViewById(R.id.disabilitySpinner);
+                ArrayAdapter<String> disabilityAdapter = new ArrayAdapter<String>(ListCategoriesScreenActivity.this,
+                        android.R.layout.simple_spinner_item, disabilities);
+                disabilityAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+
+                disabilitySpinner.setAdapter(disabilityAdapter);
+
+                disabilitySpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+                    @Override
+                    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                        wyborNiepelnosprawnosci = parent.getItemAtPosition(position).toString();
+
+                    }
+
+                    @Override
+                    public void onNothingSelected(AdapterView<?> parent) {
+
+                    }
+                });
             }
 
             @Override
@@ -89,44 +130,25 @@ public class ListCategoriesScreenActivity extends AppCompatActivity {
             }
         });
 
-        ArrayAdapter<String> spinnerArrayAdapter = new ArrayAdapter<String>(this,
-                android.R.layout.simple_spinner_item, categories);
-        spinnerArrayAdapter.setDropDownViewResource(android.R.layout.simple_list_item_1);
-        categorySpinner.setAdapter(spinnerArrayAdapter);
 
-        ArrayAdapter<String> spinnerArrayAdapter2 = new ArrayAdapter<String>(this,
-                android.R.layout.simple_spinner_item, disabilities);
-        spinnerArrayAdapter2.setDropDownViewResource(android.R.layout.simple_list_item_1);
-        disabilitySpinner.setAdapter(spinnerArrayAdapter2);
-        spinnerArrayAdapter.notifyDataSetChanged();
-        spinnerArrayAdapter2.notifyDataSetChanged();
 
-        categorySpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-            @Override
-            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-                choice = parent.getItemAtPosition(position).toString();
-            }
 
-            @Override
-            public void onNothingSelected(AdapterView<?> parent) {
 
-            }
-        });
 
-        disabilitySpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-            @Override
-            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-                wyborNiepelnosprawnosci = parent.getItemAtPosition(position).toString();
-            }
-
-            @Override
-            public void onNothingSelected(AdapterView<?> parent) {
-
-            }
-        });
         searchButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+
+                Log.v("wybor",choice);
+                Log.v("wybor",wyborNiepelnosprawnosci);
+
+
+                Toast.makeText(ListCategoriesScreenActivity.this,
+                        "OnClickListener : " +
+                                "\nSpinner 1 : " + String.valueOf(categorySpinner.getSelectedItem()) +
+                                "\nSpinner 2 : " + String.valueOf(disabilitySpinner.getSelectedItem()),
+                        Toast.LENGTH_SHORT).show();
+
 
 
                 Intent intent = new Intent(ListCategoriesScreenActivity.this, PlacesListActivity.class);
@@ -136,6 +158,9 @@ public class ListCategoriesScreenActivity extends AppCompatActivity {
             }
         });
 
-
     }
+
+
+
+
 }
